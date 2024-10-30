47adc7e11a3d9b4b9d3ba7bc57500d0cef67892d
TLLRSK <tullarisk@gmail.com>
2024-10-30 19:33:45 +0100

    removed fonts import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Orienta, Comme } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/global/Footer";
 import Providers from "./providers";
